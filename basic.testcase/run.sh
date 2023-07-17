ed05a097150b9090af74001c5a527d5b8bf93ee1
Keiichi TABATA <tabata.keiichi@gmail.com>
2023-07-18 00:02:50 +0900

    Fixed script

--- basic.testcase/run.sh
+++ basic.testcase/run.sh
@@ -6,11 +6,12 @@ replay_cmd=`readlink -f ../../build/linux-x86_64-replay/suika-replay`
 compare_cmd=`readlink -f ../../build/linux-x86_64-replay/compare.py`
 
 for dir in `ls -d *.record`; do
-    echo Running $dir
+    echo Running $dir.
     dir=`readlink -f $dir`
     cd game && \
     rm -rf replay && \
     $replay_cmd $dir && \
     python3 $compare_cmd $dir replay \
     rm -rf replay sav log.txt
+    echo Passed.
 done
--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 set -eu
 
 for dir in `ls -d *.testcase`; do
-    echo Entering directory $dir
+    echo Entering directory $dir.
     cd $dir && ./run.sh
+    echo Passed.
 done
+
+echo All passed.
